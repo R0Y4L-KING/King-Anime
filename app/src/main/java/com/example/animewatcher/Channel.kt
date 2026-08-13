3ad77d68ef2f507a0f25bf76d6b2c878721091d3
R0Y4L-KING <harshitkumar21212@gmail.com>
2026-08-13 22:23:29 +0530

    Delete app/src/main directory

--- app/src/main/java/com/example/animewatcher/Channel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.animewatcher
-
-data class Channel(
-    val pckg_id: String,
-    val base_domain: String,
-    val home_url: String,
-    val mega_domains: List<String>?,
-    val name: String
-)
